a6a5e526f771be801312b7fde0e32d14ecea4b86
Glitch (hello-express) <none>
2020-09-10 03:56:30 +0000

    🕶👕 Checkpoint ./server.js:22220554/1275

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const rp = require('request-promise-native');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,14 +12,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-app.get("/", (request, response) => {
+app.get("/",function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
 
 app.post("/Dialogflow", function(request, response){
   var intentName = request.body.queryResult.intent.displayName;
-})
+  
+  if (intentName == "Teste Glitch"){
+    response.json({ "fulfillmentText" : "Isso aqui é um teste de api"});
+  }
+  
+    if (intentName == "ConsultaSaldo"){
+
+
+      function get_products(agent){
+      var url = 'https://705861b5.ngrok.io/products';
+      var options = {
+      uri: url,
+      json: true
+    };
+    return rp.get( options )
+      .then( body => {
+        agent.add("Got a response: "+body.product_name);
+      })
+      .error( err => {
+        agent.add("Got an error: ");
+    });
+}
+    response.json({ "fulfillmentText" : "Isso aqui é um teste de api"});
+  }
+  
+});
 
 
 const listener = app.listen(process.env.PORT, () => {
