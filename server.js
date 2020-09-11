a676be590555f9f302d155529986d58f0eec39ac
Glitch (hello-express) <none>
2020-09-11 18:49:16 +0000

    👏🌂 Checkpoint ./server.js:22220554/1505

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const http = require('http')
-const options = {
-  hostname: 'ec2-3-136-20-161.us-east-2.compute.amazonaws.com',
+
+
+const getSaldo = {
+  hostname: 'ec2-3-15-145-39.us-east-2.compute.amazonaws.com',
   port: 8080,
   path: '/saldo',
   method: 'GET'
 }
 
+const getDados = {
+  hostname: 'ec2-3-15-145-39.us-east-2.compute.amazonaws.com',
+  port: 8080,
+  path: '/dados',
+  method: 'GET'
+}
+
+const getExtrato = {
+  hostname: 'ec2-3-15-145-39.us-east-2.compute.amazonaws.com',
+  port: 8080,
+  path: '/extrato',
+  method: 'GET'
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -23,15 +38,18 @@ app.get("/",function(request, response) {
 });
 
 
+
 app.post("/Dialogflow", function(request, response){
   var intentName = request.body.queryResult.intent.displayName;
   
+  //Teste do glitch
   if (intentName == "Teste Glitch"){
     response.json({ "fulfillmentText" : "Isso aqui é um teste de api"});
   }
   
+  //Consulta Saldo
   if (intentName == "ConsultaSaldo"){
-  const req = http.request(options, res => {
+  const req = http.request(getSaldo, res => {
   console.log(`statusCode: ${res.statusCode}`)
 
   res.on('data', d => {
@@ -48,6 +66,43 @@ req.on('error', error => {
 req.end()
   }
   
+  //Consulta Dados
+  if (intentName == "ConsultaDados"){
+  const req = http.request(getDados, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+    response.json({ "fulfillmentText" : "Aqui estão os dados da sua conta: "+ d});
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+  response.json({ "fulfillmentText" : "Deu ruim " + error});
+})
+
+req.end()
+  }
+  
+  //Consulta Dados
+  if (intentName == "ConsultaExtrato"){
+  const req = http.request(getExtrato, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+    response.json({ "fulfillmentText" : "Aqui esta o extrato solicitado: "+ d});
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+  response.json({ "fulfillmentText" : "Deu ruim " + error});
+})
+
+req.end()
+  }
   
 });
 
